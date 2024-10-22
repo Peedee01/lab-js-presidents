6ee281a4188836a10508ee3b5ea728cc17c64ba2
Peedee01 <piethein@schouten.nl>
2024-10-22 15:47:43 +0200

    iteration6

--- src/presidents.js
+++ src/presidents.js
@@ -470,19 +470,34 @@ function sortPresidentsByBirthYear(presidentsArr) {
 }
 
 
-
-
 // Bonus: Iteration 5 | Age at Inauguration - `map()`
-function getAgeAtInauguration(presidentsArr) {}
+function getAgeAtInauguration(presidentsArr) {
+  const presidentsArr2 = presidentsArr.map((president) => {
+    
+    return  {
+      id: president.id,
+      name: president.name,
+      birthYear: president.birthYear,
+      deathYear: president.deathYear,
+      tookOffice: president.tookOffice,
+      leftOffice: president.leftOffice,
+      party: president.party,
+      ageAtInauguration: president.tookOffice - president.birthYear
+    }
+  })
+  return presidentsArr2
+}
 
 
 
 
 // Bonus: Iteration 6 | Presidents Born After - `filter()`
-function getPresidentsBornAfter(presidentsArr, year) {}
-
-
-
+function getPresidentsBornAfter(presidentsArr, year) {
+  const presidentsArrBornAfter = presidentsArr.filter((president) => {
+    return president.birthYear > year;
+  })
+  return presidentsArrBornAfter;
+} 
 
 // Bonus: Iteration 7 | Count Republican Presidents
 function countRepublicanPresidents(presidentsArr) {}
